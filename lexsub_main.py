6d24d0c8f72897bd940682dc151d1460477540c9
casey <casey.oneill9@gmail.com>
2022-08-05 22:29:41 +0000

    starting hw4

--- lexsub_main.py
+++ lexsub_main.py
@@ -2,9 +2,9 @@
 import sys
 
 from lexsub_xml import read_lexsub_xml
-from lexsub_xml import Context 
+from lexsub_xml import Context
 
-# suggested imports 
+# suggested imports
 from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
 
@@ -12,20 +12,20 @@ import numpy as np
 import tensorflow
 
 import gensim
-import transformers 
+import transformers
 
 from typing import List
 
-def tokenize(s): 
+def tokenize(s):
     """
-    a naive tokenizer that splits on punctuation and whitespaces.  
+    a naive tokenizer that splits on punctuation and whitespaces.
     """
-    s = "".join(" " if x in string.punctuation else x for x in s.lower())    
-    return s.split() 
+    s = "".join(" " if x in string.punctuation else x for x in s.lower())
+    return s.split()
 
 def get_candidates(lemma, pos) -> List[str]:
     # Part 1
-    return [] 
+    return []
 
 def smurf_predictor(context : Context) -> str:
     """
@@ -37,13 +37,13 @@ def wn_frequency_predictor(context : Context) -> str:
     return None # replace for part 2
 
 def wn_simple_lesk_predictor(context : Context) -> str:
-    return None #replace for part 3        
-   
+    return None #replace for part 3
+
 
 class Word2VecSubst(object):
-        
+
     def __init__(self, filename):
-        self.model = gensim.models.KeyedVectors.load_word2vec_format(filename, binary=True)    
+        self.model = gensim.models.KeyedVectors.load_word2vec_format(filename, binary=True)
 
     def predict_nearest(self,context : Context) -> str:
         return None # replace for part 4
@@ -51,14 +51,14 @@ class Word2VecSubst(object):
 
 class BertPredictor(object):
 
-    def __init__(self): 
+    def __init__(self):
         self.tokenizer = transformers.DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
         self.model = transformers.TFDistilBertForMaskedLM.from_pretrained('distilbert-base-uncased')
 
     def predict(self, context : Context) -> str:
         return None # replace for part 5
 
-    
+
 
 if __name__=="__main__":
 
@@ -69,5 +69,5 @@ if __name__=="__main__":
 
     for context in read_lexsub_xml(sys.argv[1]):
         #print(context)  # useful for debugging
-        prediction = smurf_predictor(context) 
+        prediction = smurf_predictor(context)
         print("{}.{} {} :: {}".format(context.lemma, context.pos, context.cid, prediction))
